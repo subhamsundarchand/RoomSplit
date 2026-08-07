c115446721147af5e8c913c4145c2908003854c6
Subham Sundar Chand <subhamsundarchand@Subhams-MacBook-Air.local>
2026-08-07 22:37:51 +0530

    Configure Firebase for Render

--- backend/firebase/firebase.js
+++ backend/firebase/firebase.js
@@ -1,10 +1,15 @@
 const { initializeApp, cert } = require("firebase-admin/app");
 const { getFirestore } = require("firebase-admin/firestore");
+const path = require("path");
 
-const serviceAccount = require("./roomsplit-pro-firebase-adminsdk-fbsvc-3b32fdd793.json");
+const serviceAccount = require(
+  process.env.RENDER
+    ? "/etc/secrets/firebase-key.json"
+    : path.join(__dirname, "roomsplit-pro-firebase-adminsdk-fbsvc-3b32fdd793.json")
+);
 
 initializeApp({
-    credential: cert(serviceAccount)
+  credential: cert(serviceAccount),
 });
 
 const db = getFirestore();
