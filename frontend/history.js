8018a6731647c582e381a471cf29fe20e6eb84b1
Subham Sundar Chand <subhamsundarchand@Subhams-MacBook-Air.local>
2026-08-08 02:52:10 +0530

    Fix production API URLs

--- frontend/history.js
+++ frontend/history.js
@@ -565,8 +565,6 @@ async function confirmSettlement() {
 
         const upiLink =
 `upi://pay?pa=${upiId}&pn=${getName(receiver)}&am=${settlement.remainingAmount}&tn=RoomSplit Settlement&cu=INR`;
-console.log("UPI ID:", upiId);
-console.log("UPI LINK:", upiLink);
 
 
 
